78bf0cf0527e1422475bf727a292727d754f5ac9
Joseph Shunk <joseph.shunk@gmail.com>
2024-03-28 00:04:39 -0700

    Fixing up some more issues converting game object to entity.

--- www/js/essentialEngine/networking/chatClient.js
+++ www/js/essentialEngine/networking/chatClient.js
@@ -14,11 +14,11 @@ define(
 	const sComponentName = 'ChatClient';
 
 	// Private variables:
-	const m_gSocket = Symbol('m_gSocket');
+	const m_eSocket = Symbol('m_eSocket');
 	const m_sSendEvent = Symbol('m_sSendEvent');
 	const m_sReceiveEvent = Symbol('m_sReceiveEvent');
-	const m_gChatBox = Symbol('m_gChatBox');
-	const m_gInputBox = Symbol('m_gInputBox');
+	const m_eChatBox = Symbol('m_eChatBox');
+	const m_eInputBox = Symbol('m_eInputBox');
 
 	class ChatClient extends Component
 	{
@@ -30,10 +30,10 @@ define(
 		preInit(callback)
 		{
 			UTIL.log('ChatClient preInit');
-			if (this[m_gSocket] && this[m_sReceiveEvent] && this[m_gChatBox])
+			if (this[m_eSocket] && this[m_sReceiveEvent] && this[m_eChatBox])
 			{
 				UTIL.log('ChatClient listening');
-				this[m_gSocket].listen(this[m_sReceiveEvent], this.onChatMessage.bind(this));
+				this[m_eSocket].listen(this[m_sReceiveEvent], this.onChatMessage.bind(this));
 			}
 			callback();
 		}
@@ -43,9 +43,9 @@ define(
 		onKeydown(e)
 		{
 			let bSuccess = true;
-			if (!this[m_gSocket])
+			if (!this[m_eSocket])
 			{
-				UTIL.log("Socket missing or invalid prop_g_socket");
+				UTIL.log("Socket missing or invalid prop_e_socket");
 				bSuccess = false;
 			}
 			if (!this[m_sSendEvent])
@@ -53,30 +53,30 @@ define(
 				UTIL.log("SendEvent missing or invalid prop_s_send_event");
 				bSuccess = false;
 			}
-			if (!this[m_gInputBox])
+			if (!this[m_eInputBox])
 			{
-				UTIL.log("InputBox missing or invalid prop_g_input_box");
+				UTIL.log("InputBox missing or invalid prop_e_input_box");
 				bSuccess = false;
 			}
 
 			if (bSuccess && e.key == 'Enter')
 			{
-				let sText = $(this[m_gInputBox]).val();
+				let sText = $(this[m_eInputBox]).val();
 				let sEventName = this[m_sSendEvent];
 				UTIL.log(`Sending ${sText} as event ${sEventName}`);
-				this[m_gSocket].send(sEventName, sText);
-				$(this[m_gInputBox]).val('');
+				this[m_eSocket].send(sEventName, sText);
+				$(this[m_eInputBox]).val('');
 			}
 		}
 
 		onChatMessage(sMessage)
 		{
-			$(this[m_gChatBox]).append(`<div class="chatMessage">${sMessage}</div>`);
+			$(this[m_eChatBox]).append(`<div class="chatMessage">${sMessage}</div>`);
 		}
 
-		setSocket(gSocket)
+		setSocket(eSocket)
 		{
-			this[m_gSocket] = gSocket;
+			this[m_eSocket] = eSocket;
 		}
 
 		setSendEvent(sSendEvent)
@@ -89,15 +89,15 @@ define(
 			this[m_sReceiveEvent] = sReceiveEvent;
 		}
 
-		setChatBox(gChatBox)
+		setChatBox(eChatBox)
 		{
-			this[m_gChatBox] = gChatBox;
+			this[m_eChatBox] = eChatBox;
 		}
 
-		setInputBox(gInputBox)
+		setInputBox(eInputBox)
 		{
-			this[m_gInputBox] = gInputBox;
-			$(gInputBox).keydown(this.onKeydown.bind(this));
+			this[m_eInputBox] = eInputBox;
+			$(eInputBox).keydown(this.onKeydown.bind(this));
 		}
 	}
 
